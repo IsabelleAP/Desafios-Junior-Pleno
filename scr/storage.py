488680676ab5fdfcb077f543788db507dee7ba42
Isabelle <apisabelle@outlook.com>
2026-03-11 11:26:01 -0300

    storage

--- scr/storage.py
+++ scr/storage.py
@@ -7,4 +7,3 @@ def save_json(data,path):
     with open(path, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=2)
             # ensure_ascii= False preserva os acentos
-
