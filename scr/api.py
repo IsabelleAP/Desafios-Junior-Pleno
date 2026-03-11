ab788065046568dc070c11bdbb4e5d620d2bc495
Isabelle <apisabelle@outlook.com>
2026-03-11 18:34:07 -0300

    desafio_02

--- scr/api.py
+++ scr/api.py
@@ -1,25 +1,36 @@
 import requests
 from typing import List, Dict, Any
 
-def fetch_jobs(label:str) -> List[Dict[str,Any]]:
-    headers = {"User-Agent": "Mozilla/5.0"} # requisição como se fosse um navegador
-    url = "https://portal.api.gupy.io/api/job" # endereço da api
+def fetch_jobs(label: str) -> List[Dict[str, Any]]:
+    headers = {"User-Agent": "Mozilla/5.0"}
+    url = "https://portal.api.gupy.io/api/job"
 
-    params = {
-        "name": label,
-        "offset": 0, # posição inicial da busca (página)
-        "limit": 400 # número máximo de resultados
-        }
-        
+    offset = 0
+    limit = 10
+    all_jobs: List[Dict[str, Any]] = []
+    total = None
+
     try:
-        response = requests.get(url, headers=headers, params=params, timeout=10)
-        # timeout para esperar no máximo 10 segundos pela resposta
-        response.raise_for_status()
-        # gerar erro se o servidor responder algo como 404
+        while total is None or offset < total:
+            params = {
+                "name": label,
+                "offset": offset,
+                "limit": limit
+            }
 
-        data = response.json()["data"]
+            response = requests.get(url, headers=headers, params=params, timeout=10)
+            response.raise_for_status()
 
-        return data
+            result = response.json()
+
+            data = result["data"]
+            total = result["pagination"]["total"]
+
+            all_jobs.extend(data)
+
+            offset += limit
+
+        return all_jobs
 
     except requests.exceptions.RequestException as e:
         print(f"Erro ao buscar {label}: {e}")
